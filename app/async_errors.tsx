cff6f7807930abbef565e3324a0cf86883a8d683
Lai Wei <royweilai@gmail.com>
2024-01-28 17:20:41 -0800

    update error handle logic

--- app/async_errors.tsx
+++ app/async_errors.tsx
@@ -1,3 +1,22 @@
+import axios from 'axios';
+
+export function sendErrorToEndpoint(errorName: string, stackTrace: string, additionalInfo: string) {
+  const url = 'http://127.0.0.1:8000/incident';
+  const data = {
+    error_message: errorName,
+    stack_trace: stackTrace,
+    additional_info: additionalInfo,
+  };
+
+  axios.post(url, data)
+    .then(response => {
+      console.log('Error sent successfully');
+    })
+    .catch(error => {
+      console.error('Error sending error:', error);
+    });
+}
+
 export function setupGlobalErrorHandling() {
   // Check if window is defined to ensure this code runs only in the browser
   if (typeof window !== 'undefined') {
@@ -7,10 +26,11 @@ export function setupGlobalErrorHandling() {
         message: event.reason.message || 'Unhandled rejection',
         stack: event.reason.stack,
       };
-      // Send the errorInfo to your server logging endpoint
-      // ...
+
+      sendErrorToEndpoint(errorInfo.message, errorInfo.stack, "")
+      
       // Then redirect to the error page
-      window.location.href = '/error';
+      //window.location.href = '/error';
     });
   }
 }
--- app/error.tsx
+++ app/error.tsx
@@ -1,32 +1,33 @@
 'use client' // Error components must be Client Components
- 
-import { useEffect } from 'react'
- 
+
+import { useEffect } from 'react';
+import { sendErrorToEndpoint } from './async_errors';
+
 export default function Error({
-  error,
-  reset,
+    error,
+    reset,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+    error: Error & { digest?: string };
+    reset: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
-  return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Sorry something went wrong, please contact our support!
-      </button>
-    </div>
-  )
+    useEffect(() => {
+        console.error(error);
+        sendErrorToEndpoint(error.name, error.stack, "");
+    }, [error]);
+
+    return (
+        <div>
+            <h2>Something went wrong!</h2>
+            <button
+                onClick={
+                    // Attempt to recover by trying to re-render the segment
+                    () => reset()
+                }
+            >
+                Sorry something went wrong, please contact our support!
+            </button>
+        </div>
+    );
 }
 
 
